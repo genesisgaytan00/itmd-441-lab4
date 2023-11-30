cfdc957c3b5f40c927373775a8f78e12cc415b99
genesisgaytan00 <122740973+genesisgaytan00@users.noreply.github.com>
2023-11-29 22:51:11 -0600

    change
    changee

--- js/script.js
+++ js/script.js
@@ -1,30 +1,66 @@
-//locations cordinates
+
+// locations coordinates
 const locations = {
     'Mexico City, Mexico': { lat: 19.4326, lon: -99.1332 },
     'Paris, France': { lat: 48.8534, lon: 2.3486 },
     'Positano, Italy': { lat: 40.6281, lon: 14.4842 },
     'San Juan, Puerto Rico': { lat: 18.4655, lon: -66.1057 },
-    'Chicago': { lat: 41.8781, lon: -87.6298 }
+    'Current Location': null, // Placeholder for current location
 };
 
-//event listener
+// event listener
 document.getElementById('locations').addEventListener('change', function () {
     const selectedOption = this.options[this.selectedIndex];
     const { lat, lon } = locations[selectedOption.value];
     getLocation(lat, lon);
 });
-//results display
+
+// ...
+
+// results display
 function setResults(elementId, text, color) {
     const element = document.getElementById(elementId);
     element.innerText = text;
     element.style.color = color;
 }
 
+// function to get current location
+function updateCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
 
-//getting from pi
+            // Update the 'Current Location' option directly
+            locations['Current Location'] = { lat: latitude, lon: longitude };
+            const locationsDropdown = document.getElementById('locations');
+            locationsDropdown.options[locationsDropdown.options.length - 1].value = 'Current Location';
+            locationsDropdown.options[locationsDropdown.options.length - 1].text = 'Current Location';
+            locationsDropdown.dispatchEvent(new Event('change'));
+        },
+        function (error) {
+            console.error('Error', error.message);
+        }
+    );
+}
+
+// call
+updateCurrentLocation();
+
+// getting from API
 function getLocation(lat, lon) {
-    const todayUrl = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lon}&date=today`;
-    const tomorrowUrl = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lon}&date=tomorrow`;
+    if (!lat || !lon) {
+        console.error('Invalid coordinates');
+        return;
+    }
+
+    const todayUrl = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lon}&date=today&formatted=0`;
+    const tomorrowUrl = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lon}&date=tomorrow&formatted=0`;
+
+    function formatTime(timestamp) {
+        const date = new Date(timestamp * 1000);
+        return date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+    }
 
     fetch(todayUrl)
         .then(response => {
@@ -35,10 +71,13 @@ function getLocation(lat, lon) {
         })
         .then(data => {
             console.log('Today API Response:', data);
-            setResults('todaySunrise', `${data.results.sunrise}`, 'grey');
-            setResults('todaySunset', `${data.results.sunset}`, 'grey');
-            setResults('todayDusk', ` ${data.results.civil_twilight_end}`, 'grey');
-            setResults('todayDawn', `${data.results.civil_twilight_begin}`, 'grey');
+            setResults('todaySunrise', `Sunrise: ${formatTime(data.results.sunrise)}`, 'grey');
+            setResults('todaySunset', `Sunset: ${formatTime(data.results.sunset)}`, 'grey');
+            setResults('todayDusk', `Dusk: ${formatTime(data.results.civil_twilight_end)}`, 'grey');
+            setResults('todayDawn', `Dawn: ${formatTime(data.results.civil_twilight_begin)}`, 'grey');
+            setResults('todayDayLength', `Day Length: ${data.results.day_length} seconds`, 'grey');
+            setResults('todaySolarNoon', `Solar Noon: ${formatTime(data.results.solar_noon)}`, 'grey');
+            setResults('todayTimeZone', `Time Zone: ${data.results.timezone}`, 'grey');
         })
         .catch(error => {
             console.error('Error fetching today data:', error);
@@ -53,14 +92,18 @@ function getLocation(lat, lon) {
         })
         .then(data => {
             console.log('Tomorrow API Response:', data);
-            setResults('tomorrowSunrise', `${data.results.sunrise}`, 'grey');
-            setResults('tomorrowSunset', `${data.results.sunset}`, 'grey');
-            setResults('tomorrowDusk', `${data.results.civil_twilight_end}`, 'grey');
-            setResults('tomorrowDawn', `${data.results.civil_twilight_begin}`, 'grey');
+            setResults('tomorrowSunrise', `Sunrise: ${formatTime(data.results.sunrise)}`, 'grey');
+            setResults('tomorrowSunset', `Sunset: ${formatTime(data.results.sunset)}`, 'grey');
+            setResults('tomorrowDusk', `Dusk: ${formatTime(data.results.civil_twilight_end)}`, 'grey');
+            setResults('tomorrowDawn', `Dawn: ${formatTime(data.results.civil_twilight_begin)}`, 'grey');
+            setResults('tomorrowDayLength', `Day Length: ${data.results.day_length} seconds`, 'grey');
+            setResults('tomorrowSolarNoon', `Solar Noon: ${formatTime(data.results.solar_noon)}`, 'grey');
+            setResults('tomorrowTimeZone', `Time Zone: ${data.results.timezone}`, 'grey');
         })
         .catch(error => {
             console.error('Error fetching tomorrow data:', error);
         });
 }
-//call
-getLocation(locations['Mexico City, Mexico'].lat, locations['Mexico City, Mexico'].lon);
+
+// calling Mexico City
+getLocation(19.4326, -99.1332);
